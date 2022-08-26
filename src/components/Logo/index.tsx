b5546b60179274e0ca343252f69fd00763f31005
Dailton Bastos <dailtonbastos@gmail.com>
2022-08-26 08:38:15 -0300

    feat: add logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+import { ReactComponent as LogoIcon } from '~/assets/logo.svg';
+
+export const Logo = () => {
+  return (
+    <Box>
+      <LogoIcon />
+    </Box>
+  );
+};
