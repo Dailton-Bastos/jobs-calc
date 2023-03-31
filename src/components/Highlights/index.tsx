248331779e1831722622d41e91812a2b60f641f5
Dailton Bastos <dailtonbastos@gmail.com>
2023-03-30 23:07:32 -0300

    feat(highlights): filter jobs highlights

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -42,6 +42,10 @@ export const Highlights = () => {
   const { cyclesByUser } = useCyclesContext();
   const { getTotalHoursUsedActiveCycleJob } = useCycle();
 
+  const jobsFiltered = React.useMemo(() => {
+    return jobs?.filter((job) => job?.isHighlight);
+  }, [jobs]);
+
   const getJobTotalHoursUsed = React.useCallback(
     (cycles: Cycle[]) => {
       const { totalHoursUsedActiveCycleJob } =
@@ -57,7 +61,7 @@ export const Highlights = () => {
   );
 
   const getJobsHighlights = React.useCallback(() => {
-    const highlights = [...jobs]?.map((job) => {
+    const highlights = [...jobsFiltered]?.map((job) => {
       const cycles = cyclesByUser?.filter((cycle) => cycle?.jobId === job?.id);
 
       const { hours, minutes, totalHoursUsed } = getJobTotalHoursUsed(cycles);
@@ -88,7 +92,7 @@ export const Highlights = () => {
     });
 
     return { highlights };
-  }, [cyclesByUser, jobs, getJobTotalHoursUsed]);
+  }, [cyclesByUser, jobsFiltered, getJobTotalHoursUsed]);
 
   React.useEffect(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
@@ -103,65 +107,71 @@ export const Highlights = () => {
 
   return (
     <>
-      <TableContainer>
-        <Table colorScheme="blackAlpha">
-          <Thead>
-            <Tr>
-              <Th>Título</Th>
-              <Th>Tempo Estimado</Th>
-              <Th>Tempo Utilizado</Th>
-              <Th>Status</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {jobsHighlights?.map((job) => (
-              <Tr key={job?.id}>
-                <Td>
-                  <Tooltip label={job?.title} placement="top-start">
-                    <LinkChakra as={Link} to={`/jobs/${job?.id}`}>
-                      {truncateString(job?.title, 55)}
-                    </LinkChakra>
-                  </Tooltip>
-                </Td>
+      {!jobsHighlights.length ? (
+        <Text textAlign="center" fontSize="large" mt="10">
+          Nenhum resultado encontrado
+        </Text>
+      ) : (
+        <TableContainer>
+          <Table colorScheme="blackAlpha">
+            <Thead>
+              <Tr>
+                <Th>Título</Th>
+                <Th>Tempo Estimado</Th>
+                <Th>Tempo Utilizado</Th>
+                <Th>Status</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {jobsHighlights?.map((job) => (
+                <Tr key={job?.id}>
+                  <Td>
+                    <Tooltip label={job?.title} placement="top-start">
+                      <LinkChakra as={Link} to={`/jobs/${job?.id}`}>
+                        {truncateString(job?.title, 55)}
+                      </LinkChakra>
+                    </Tooltip>
+                  </Td>
 
-                <Td>{job?.estimatedTime}</Td>
+                  <Td>{job?.estimatedTime}</Td>
 
-                <Td>
-                  <Text
-                    fontSize="md"
-                    color={STATUS_COLORS[job?.usedTime?.statusColor]}
-                  >
-                    {job?.usedTime?.time}
-                  </Text>
-                </Td>
-
-                <Td>
-                  <Flex gap="2" align="center" justify="flex-start">
-                    <Box
-                      w="8px"
-                      h="8px"
-                      borderRadius="50%"
-                      bg={STATUS_COLORS[job.status.statusColor]}
-                    />
-
+                  <Td>
                     <Text
                       fontSize="md"
-                      color={STATUS_COLORS[job.status.statusColor]}
+                      color={STATUS_COLORS[job?.usedTime?.statusColor]}
                     >
-                      {job.status.type}
+                      {job?.usedTime?.time}
                     </Text>
-                  </Flex>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+                  </Td>
+
+                  <Td>
+                    <Flex gap="2" align="center" justify="flex-start">
+                      <Box
+                        w="8px"
+                        h="8px"
+                        borderRadius="50%"
+                        bg={STATUS_COLORS[job.status.statusColor]}
+                      />
+
+                      <Text
+                        fontSize="md"
+                        color={STATUS_COLORS[job.status.statusColor]}
+                      >
+                        {job.status.type}
+                      </Text>
+                    </Flex>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      )}
 
       <Box mt="5">
         <Pagination
           currentPage={currentPage}
-          totalCount={jobs?.length}
+          totalCount={jobsFiltered?.length}
           pageSize={PageSize}
           siblingCount={1}
           onPageChange={(page) => setCurrentPage(page)}
