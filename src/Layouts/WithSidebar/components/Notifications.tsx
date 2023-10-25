406f07cb407d658aaeb60d18550a90b235b59a6c
Dailton Bastos <dailtonbastos@gmal.com>
2023-10-24 21:16:21 -0300

    feat(notifications): add pulse effect

--- src/Layouts/WithSidebar/components/Notifications.tsx
+++ src/Layouts/WithSidebar/components/Notifications.tsx
@@ -10,13 +10,44 @@ import {
   PopoverCloseButton,
   Button,
   Text,
+  Box,
+  keyframes,
 } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
 export const Notifications = () => {
+  const animationKeyframes = keyframes`
+  0% { transform: scale(0.95); box-shadow: 0 0 0 0 rgba(255, 121, 63, 0.7); }
+  70% { transform: scale(1); box-shadow: 0 0 0 10px rgba(255, 121, 63, 0); }
+  100% { transform: scale(0.95); box-shadow: 0 0 0 0 rgba(255, 121, 63, 0); }
+`;
+
+  const animation = `${animationKeyframes} 2s infinite`;
+
   return (
     <Popover isLazy>
       <PopoverTrigger>
-        <Button bg="transparent">
+        <Button bg="transparent" position="relative">
+          <Box
+            as={motion.div}
+            border="2px"
+            borderColor="gray.50"
+            position="absolute"
+            top="8px"
+            right="15px"
+            borderRadius="full"
+            boxShadow="0 0 0 0 rgba(255, 121, 63, 0.7)"
+            scale={1}
+            animation={animation}
+          >
+            <Box
+              w="8px"
+              h="8px"
+              bg="rgba(255, 121, 63, 1)"
+              borderRadius="full"
+            />
+          </Box>
+
           <BsBellFill size={24} />
         </Button>
       </PopoverTrigger>
